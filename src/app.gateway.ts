33408fe8e20af37e0c947f61db38104ae0216e8c
potterbl <zeexample42@gmail.com>
2023-09-10 16:03:54 +0300

    cors

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,7 +1,7 @@
 import {SubscribeMessage, WebSocketGateway, WebSocketServer} from '@nestjs/websockets';
 import {Server, Socket} from "socket.io";
 
-@WebSocketGateway()
+@WebSocketGateway({ cors: true })
 export class AppGateway {
   @WebSocketServer()
   server: Server
